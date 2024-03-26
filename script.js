659840cf9b632c516e928f712e8617f4911eb8ae
JulieKodehode <julie.kodehode@gmail.com>
2024-03-26 12:03:08 +0100

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,70 @@
 console.log('Hello!');
+
+// Regular Functions / Functions
+
+function greeting() {
+    console.log("Dette er en funksjon!");
+
+    const firstName = "Julie";
+    const greet = "Good morning";
+    
+    console.log(greet + " " + firstName)
+    console.log(`${greet}, ${firstName}!`)
+}
+
+// greeting();
+
+// DOMM (Document Object Model Manipulation)
+
+// lagringsplass = lagringsinnhold
+const greetingButton = document.querySelector("#greetingButton");
+console.log(greetingButton);
+
+greetingButton.addEventListener("click", greeting);
+
+
+// HENT
+const themeSwitchButton = document.querySelector("#themeSwitchButton");
+console.log(themeSwitchButton);
+
+// MODIFISER
+function themeSwitch() {
+    // HENT
+    const body = document.querySelector("body");
+    console.log(body);
+
+    // MODIFISER
+    body.classList.toggle("dark");
+}
+
+// SEND
+themeSwitchButton.addEventListener("click", themeSwitch);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+greetingButton.addEventListener("click", () => {
+    console.log("Dette er en arrow function");
+
+    const firstName = "Julie";
+    const greet = "Good morning";
+    console.log(greet + " " + firstName)
+});
+*/
